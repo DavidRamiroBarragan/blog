1327d23c0bad9b2c0b8972657cfaf423b0ef2e9a
David Ramiro Barragan <dramirobarragan@gmail.com>
2019-10-18 14:47:11 +0200

    create project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <p>En construcción</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
