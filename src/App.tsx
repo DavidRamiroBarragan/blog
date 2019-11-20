69ca1fcb05e2d2282c6facf00f70e3661017f5c8
David Ramiro Barragan <dramirobarragan@gmail.com>
2019-11-20 21:12:54 +0100

    add eslint, husky, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
-const App: React.FC = () => {
+const App: React.FC = (): JSX.Element => {
+  const [state, setState] = useState('');
+  const hola = () => console.log(hola);
+  useEffect(() => {
+    setState('hola');
+    hola();
+  }, [setState, state]);
   return (
     <div className="App">
       <header className="App-header">
-         <p>En construcción</p>
+        <p>En construcción</p>
       </header>
     </div>
   );
