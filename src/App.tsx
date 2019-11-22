6253b0817476e014145a7d6e1268b681f0ebebcf
dramiro <dramirobarragan@gmail.com>
2019-11-22 20:55:27 +0100

    modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import './App.scss';
 
 const App: React.FC = (): JSX.Element => {
-  const [state, setState] = useState('');
-  const hola = useCallback((): void => console.log(hola), []);
-  useEffect(() => {
-    setState('hola');
-    hola();
-  }, [hola, setState, state]);
   return (
     <div className="app">
       <header className="app-header">
